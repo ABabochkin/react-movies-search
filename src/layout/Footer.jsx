68aecddc7fa9c2c3f4242b0f6273a3034bc59320
PrettyFlacko777 <aababochkin45@gmail.com>
2022-09-06 16:32:09 +0300

    project complete

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="page-footer blue accent-1 ">
+      <div className="footer-copyright">
+        <div className="container">
+          © 2022
+          <a className="grey-text text-lighten-3 right" href="#!">
+            More Links
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export { Footer };
---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <nav className="indigo accent-3">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          Кинопоиск
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Gang</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export { Header };
